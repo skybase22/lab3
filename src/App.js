bffa98759d38a0313b39f99ca8ae160f2de6178f
nut <ynotmela@gmail.com>
2018-09-22 19:42:21 +0700

    3

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+const word = "Hello";
+
 class App extends Component {
   render() {
     return (
       <div>
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
+          {
+            Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+          }
       </div>
-
           );
         }
       }
