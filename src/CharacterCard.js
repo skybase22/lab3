c9c525e8f0cba26e2071d1e5fc0763bd894e972b
nut <ynotmela@gmail.com>
2018-09-22 19:33:11 +0700

    2

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
 export default class CharacterCard extends Component {
     render(){
-        return (<div className = "card">{this.props.value}</div>)
+    return (
+    <div>{this.props.value}</div>
+    )
     }
    }
